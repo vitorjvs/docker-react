4d63fbd75f07694ae4995e963a5c53eec6d0234c
vitorjvs <vitor.sfc99@hotmail.com>
2022-07-19 22:13:37 -0300

    first commit

---
+++ my-app/src/App2.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App2 extends Component {
+    render() {
+    
+        return (
+            <div>
+                <h1 className='text-info'>Teste</h1>
+            </div>
+        );
+
+    }
+
+}
+
+export default App2;
